2b3d179c93349ae599ad8647b6f004aebf1ae37e
Micah Ferrill <mcferrill@gmail.com>
2017-06-09 10:56:03 -0500

    Added time output to events tool.

--- src/c10-events.py
+++ src/c10-events.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 
+from datetime import timedelta
 import sys
 
 from chapter10 import C10
+from chapter10.datatypes import Time
+
+
+def abs_time(rtc, time_packet):
+    time_packet.body.parse()
+    offset = (rtc - time_packet.rtc) / 10000000.0
+    return time_packet.body.time + timedelta(seconds=offset)
 
 
 def main():
     if len(sys.argv) < 2:
         print 'usage: c10-events <input_file>'
 
+    last_time = None
+
     for packet in C10(sys.argv[-1]):
+        if isinstance(packet.body, Time):
+            last_time = packet
+            continue
+
         if packet.data_type == 0x2:
-            print 'Recording Event packet at %s' % packet.rtc
+            print 'Recording Event packet at %s' % abs_time(
+                packet.rtc, last_time)
             for i, entry in enumerate(packet.body):
                 print '    %s of %s at %s' % (
                     i + 1, packet.body.recording_event_entry_count,
-                    entry.intra_packet_timestamp)
+                    abs_time(entry.intra_packet_timestamp, last_time))
                 print '    Occurrence: %s' % entry.event_occurrence
                 print '    Count: %s' % entry.event_count
                 print '    Number: %s' % entry.event_number
