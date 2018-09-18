c4857679f0143289c3165c4e4662edbe6d95197f
Micah Ferrill <mcferrill@gmail.com>
2018-09-18 16:42:10 -0500

    WIP

--- src/c10-dump-pcap.py
+++ src/c10-dump-pcap.py
@@ -9,10 +9,14 @@ Options:
     -f --force  Overwrite existing files."""
 
 from time import mktime
+import struct
 import os
 
 from docopt import docopt
 from dpkt.pcap import Writer
+from dpkt.udp import UDP
+from dpkt.ip import IP
+from dpkt.ethernet import Ethernet
 # from i106 import C10
 from chapter10 import C10
 
@@ -51,6 +55,24 @@ if __name__ == '__main__':
                 continue
 
             for msg in packet:
+                data = msg.data
+                if packet.data_type == 0x69:
+                    udp = UDP(sport=msg.src_port,
+                              dport=msg.dst_port,
+                              data=data,
+                              ulen=len(data))
+                    ip = IP(data=udp,
+                            len=len(data),
+                            src=bytes(msg.source_ip),
+                            dst=bytes(msg.dest_ip))
+                    print(type(struct.pack('=IH', 50331648, msg.virtual_link)))
+                    ethernet = Ethernet(
+                        data=ip,
+                        # len=len(ip),
+                        dst=struct.pack('=IH', 50331648, msg.virtual_link))
+
+                    data = str(ethernet)
+
                 t = get_time(msg.intra_packet_timestamp, last_time)
                 t = mktime(t.timetuple()) + (t.microsecond/1000000.0)
-                writer.writepkt(msg.data, t)
+                writer.writepkt(data, t)
