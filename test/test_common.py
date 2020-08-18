29091983d338f29476db49e9ba75eb923ca2a2f4
Micah Ferrill <mcferrill@gmail.com>
2020-08-18 15:42:02 -0500

    Changed test to use a set.

--- test/test_common.py
+++ test/test_common.py
@@ -10,7 +10,7 @@ from src import common
 def test_find_c10():
     dirname = os.path.dirname(__file__)
     result = common.find_c10([dirname])
-    assert list(result) == [pytest.SAMPLE, pytest.EVENTS]
+    assert set(result) == set([pytest.SAMPLE, pytest.EVENTS])
 
 
 def test_fmt_table():
