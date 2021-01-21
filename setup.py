fdf58729100630855b168fb47fe31910cf0f224f
Micah Ferrill <mcferrill@gmail.com>
2021-01-21 14:38:19 -0600

    Dropped support for python 3.6 for s3fs compatibility.

--- setup.py
+++ setup.py
@@ -102,7 +102,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries",
