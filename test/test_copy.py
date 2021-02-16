0b8f57ef894c86f0dca491ab4a4087264d741e18
Micah Ferrill <mcferrill@gmail.com>
2021-02-16 13:23:20 -0600

    Fixed fixture names.

--- test/test_copy.py
+++ test/test_copy.py
@@ -9,12 +9,12 @@ from c10_tools.c10_copy import wrapper as main
 def test_overwrite():
     with NamedTemporaryFile() as out:
         with pytest.raises(SystemExit):
-            main((pytest.SOURCE, out.name))
+            main((pytest.SAMPLE, out.name))
 
 
 def test_noargs():
     path = NamedTemporaryFile().name
     with open(path, 'w+b') as out:
-        main((pytest.SOURCE, path, '-f'))
+        main((pytest.SAMPLE, path, '-f'))
         out.seek(0)
-        assert out.read() == open(pytest.SOURCE, 'rb').read()
+        assert out.read() == open(pytest.SAMPLE, 'rb').read()
