87d463523930c1e1d3a5188be1b2c25df853ce30
Micah Ferrill <mcferrill@gmail.com>
2022-04-21 11:10:43 -0500

    Finished updating copy command.

--- c10_tools/copy.py
+++ c10_tools/copy.py
@@ -2,11 +2,17 @@
 from datetime import datetime, timedelta
 import os
 
+import click
+
+from chapter10.computer import ComputerF1
 from c10_tools.common import walk_packets, FileProgress, C10
 
 
 def parse_offset(s):
     """Take byte or time offset and return the correct object."""
+
+    if s is None:
+        return
 
     # Byte offset
     if s.isnumeric():
@@ -28,29 +34,25 @@ def parse_offset(s):
                          microseconds=dt.microsecond)
 
 
-def as_hex(bytes):
-    from array import array
-    return ' '.join(hex(b)[2:].zfill(2) for b in array('B', bytes))
-
-
-def main(args):
+@click.command
+@click.option('-c', '--channel', type=str, help='Specify channels (comma-separated) to include')
+@click.option('-e', '--exclude', type=str, help='Specify channels (comma-separated) to exclude')
+@click.option('-t', '--type', type=str, help='Specify datatypes (comma-separated) to include')
+@click.option('-f', '--force', is_flag=True, help='Overwrite existing files')
+@click.argument('src')
+@click.argument('dst')
+@click.argument('start', default='0')
+@click.argument('end', required=False)
+@click.pass_context
+def copy(ctx, src, dst, start, end, channel=None, exclude=None, type=None, force=False):
     """Copy a Chapter 10 file. Can selectively copy by channel, type, byte \
-offset, or time.
-    copy <src> <dst> [options]
-    copy <src> <dst> <end> [options]
-    copy <src> <dst> <start> <end> [options]
-    -c CHANNEL..., --channel CHANNEL...  Specify channels to include (comma \
-separated).
-    -e CHANNEL..., --exclude CHANNEL...  Specify channels to ignore (comma \
-separated).
-    -t TYPE, --type TYPE  The types of data to copy (comma separated, may be \
-decimal or hex eg: 0x40)
-    -f --force  Overwrite existing files.
-    -s, --slice  Slice a file based on time or byte offset.
+    offset, or time.
 
-    Optional "<start>" is offset into file to start from. "<end>" is the offset
-    from "<start>" or file start to trim to. "<start>" and "<end>" can be any
+    Optional "START" is offset into file to start from. "END" is the offset
+    from "START" or file start to trim to. "START" and "END" can be any
     combination of:
+
+    \b
         byte offset
         relative time (HH:MM:SS.ms)
         absolute time (DDD:HH:MM:SS.ms) where DDD is julian day.
@@ -59,48 +61,56 @@ decimal or hex eg: 0x40)
     # TODO: make dst optional or add 'stdout' option?
 
     # Don't overwrite unless explicitly required.
-    if os.path.exists(args['<dst>']) and not args['--force']:
-        print('dst file already exists. Use -f to overwrite.')
+    if os.path.exists(dst) and not force:
+        click.echo('dst file already exists. Use -f to overwrite.')
         raise SystemExit
 
-    start, end = 0, None
-    if args.get('<end>'):
-        end = parse_offset(args['<end>'])
-    if args.get('<start>'):
-        start = parse_offset(args['<start>'])
+    start = parse_offset(start)
+    end = parse_offset(end)
 
     if isinstance(end, int) and isinstance(start, int):
         end += start
 
     # Timestamp of offset 0 into the file
-    start_time = None
+    file_start_time = None
     slice_start = None
 
-    with open(args['<dst>'], 'wb') as out, FileProgress(args['<src>']) \
-            as progress:
+    with open(dst, 'wb') as out, FileProgress(src) as progress:
 
         # Iterate over packets based on args.
         offset = 0
-        for packet in walk_packets(C10(args['<src>']), args, include_time=False):
+        args = {
+            '--type': type,
+            '--channel': channel,
+            '--exclude': exclude,
+        }
+        for packet in walk_packets(C10(src), args, include_time=False):
             progress.update(packet.packet_length)
 
-            if not start_time and packet.data_type == 0x11:
-                start_time = packet.time
+            # First time packet
+            if not file_start_time and packet.data_type == 0x11:
+                file_start_time = packet.time
 
                 if isinstance(start, timedelta):
                     start += packet.time
+                elif isinstance(start, datetime):
+                    start = start.replace(year=packet.time.year)
 
                 if isinstance(end, timedelta):
                     if isinstance(start, datetime):
                         end += start
                     else:
-                        end += start_time
+                        end += file_start_time
 
-                if isinstance(end, datetime):
+                elif isinstance(end, datetime):
                     end = end.replace(year=packet.time.year)
 
+            # Can't check time until we find a time packet. Preserve TMATS
+            if start and not file_start_time and not isinstance(packet, ComputerF1):
+                pass
+
             # If we haven't reached start yet
-            if isinstance(start, int) and offset < start:
+            elif isinstance(start, int) and offset < start:
                 pass
             elif isinstance(start, datetime) and packet.get_time() < start:
                 pass
@@ -114,7 +124,7 @@ decimal or hex eg: 0x40)
                 # Slice
                 elif slice_start is not None:
                     break
-            elif start_time and isinstance(end, datetime) and \
+            elif file_start_time and isinstance(end, datetime) and \
                     packet.get_time() > end:
                 break
             else:
--- tests/test_copy.py
+++ tests/test_copy.py
@@ -5,9 +5,10 @@ from unittest.mock import patch
 from io import BytesIO
 import os
 
+from click.testing import CliRunner
 import pytest
 
-from c10_tools.copy import main
+from c10_tools.copy import copy
 from c10_tools.common import C10
 
 
@@ -30,133 +31,106 @@ def fake_open(path, mode):
     return open(path, mode)
 
 
-def test_overwrite(args):
-    args['<src>'] = pytest.SAMPLE
-    main(args)
-    assert os.stat(args['<dst>']).st_size == \
+def test_overwrite():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.SAMPLE, path, '-f'])
+    assert os.stat(path).st_size == \
         os.stat(pytest.SAMPLE).st_size
 
 
-def test_checks_exists(args):
-    args['--force'] = False
-    with patch('c10_tools.copy.open', fake_open):
-        with open(args['<dst>'], 'w+b'), pytest.raises(SystemExit):
-            main(args)
+def test_checks_exists():
+    path = NamedTemporaryFile().name
+    with open(path, 'w+b'):
+        result = CliRunner().invoke(copy, [pytest.SAMPLE, path])
+    assert 'file already exists' in result.stdout
 
 
-def test_trim_abs(args):
-    args['<end>'] = '290:22:19:23'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:22.998157'
+def test_trim_abs():
+    path = NamedTemporaryFile().name
+    with open(path, 'w+b') as f:
+        CliRunner().invoke(copy, [pytest.ETHERNET, path, '0', '290:22:19:23', '-f'])
+        packets = list(C10(f))
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:22.998157'
 
 
-def test_trim_rel(args):
-    args['<end>'] = '0:02'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
+def test_trim_rel():
+    path = NamedTemporaryFile().name
+    with open(path, 'w+b') as out:
+        CliRunner().invoke(copy, [pytest.ETHERNET, path, '0', '0:02', '-f'])
+        packets = list(C10(out))
         assert packets[-1].get_time() - packets[1].time < timedelta(seconds=2)
 
 
-def test_trim_offset(args):
-    args['<end>'] = '10000'
-    with open(args['<dst>'], 'w+b'):
-        main(args)
-        assert os.stat(args['<dst>']).st_size < 10_000
+def test_trim_offset():
+    path = NamedTemporaryFile().name
+    with open(path, 'w+b'):
+        CliRunner().invoke(copy, [pytest.ETHERNET, path, '0', '10000', '-f'])
+        assert os.stat(path).st_size < 10_000
 
 
-def test_slice_abs(args):
-    args['<start>'] = '290:22:19:23'
-    args['<end>'] = '290:22:19:24'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[1].time) == '2018-10-17 22:19:22'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:23.988158'
+def test_slice_abs():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '290:22:19:23', '290:22:19:24', '-f'])
+    packets = list(C10(path))
+    assert str(packets[1].time) == '2018-10-17 22:19:23'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:23.988158'
 
 
-def test_slice_abs_rel(args):
-    args['<start>'] = '290:22:19:23'
-    args['<end>'] = '0:01'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[1].time) == '2018-10-17 22:19:22'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:23.988158'
+def test_slice_abs_rel():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '290:22:19:23', '0:01', '-f'])
+    packets = list(C10(path))
+    assert str(packets[1].time) == '2018-10-17 22:19:23'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:23.988158'
 
 
-def test_slice_abs_offset(args):
-    args['<start>'] = '290:22:19:23'
-    args['<end>'] = '100000'
-    with open(args['<dst>'], 'w+b'):
-        main(args)
-        assert os.stat(args['<dst>']).st_size < 100_000
+def test_slice_abs_offset():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '290:22:19:23', '100000', '-f'])
+    assert os.stat(path).st_size < 100_000
 
 
-def test_slice_rel(args):
-    args['<start>'] = '0:01'
-    args['<end>'] = '0:02'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[1].time) == '2018-10-17 22:19:23'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.988158'
+def test_slice_rel():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '0:01', '0:02', '-f'])
+    packets = list(C10(path))
+    assert str(packets[1].time) == '2018-10-17 22:19:23'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.988158'
 
 
-def test_slice_rel_abs(args):
-    args['<start>'] = '0:01'
-    args['<end>'] = '290:22:19:24.988158'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[1].time) == '2018-10-17 22:19:23'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.971918'
+def test_slice_rel_abs():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '0:01', '290:22:19:24.988158', '-f'])
+    packets = list(C10(path))
+    assert str(packets[1].time) == '2018-10-17 22:19:23'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.971918'
 
 
-def test_slice_rel_offset(args):
-    args['<start>'] = '0:01'
-    args['<end>'] = '1000000'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[1].time) == '2018-10-17 22:19:23'
-        assert os.stat(args['<dst>']).st_size == 749960
+def test_slice_rel_offset():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '0:01', '1000000', '-f'])
+    packets = list(C10(path))
+    assert str(packets[1].time) == '2018-10-17 22:19:23'
+    assert os.stat(path).st_size == 749960
 
 
-def test_slice_offset(args):
-    args['<start>'] = '1000'
-    args['<end>'] = '1000000'
-    with open(args['<dst>'], 'w+b'):
-        main(args)
-        assert os.stat(args['<dst>']).st_size <= 1_000_000
+def test_slice_offset():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '1000', '1000000', '-f'])
+    assert os.stat(path).st_size <= 1_000_000
 
 
-def test_slice_offset_abs(args):
-    args['<start>'] = '1000'
-    args['<end>'] = '290:22:19:24.988158'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[0].time) == '2018-10-17 22:19:22'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.971918'
+def test_slice_offset_abs():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '1000', '290:22:19:24.988158', '-f'])
+    packets = list(C10(path))
+    assert str(packets[0].time) == '2018-10-17 22:19:22'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:24.971918'
 
 
-def test_slice_offset_rel(args):
-    args['<start>'] = '1000'
-    args['<end>'] = '0:01'
-    with open(args['<dst>'], 'w+b') as out:
-        main(args)
-        out.seek(0)
-        packets = list(C10(args['<dst>']))
-        assert str(packets[0].time) == '2018-10-17 22:19:22'
-        assert str(packets[-1].get_time()) == '2018-10-17 22:19:22.998157'
+def test_slice_offset_rel():
+    path = NamedTemporaryFile().name
+    CliRunner().invoke(copy, [pytest.ETHERNET, path, '1000', '0:01', '-f'])
+    packets = list(C10(path))
+    assert str(packets[0].time) == '2018-10-17 22:19:22'
+    assert str(packets[-1].get_time()) == '2018-10-17 22:19:22.998157'
